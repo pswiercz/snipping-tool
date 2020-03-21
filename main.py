3bd127a2e97ef640015586b861a47dd99129be5c
pswiercz <p.swiercz@student.uw.edu.pl>
2020-03-21 18:33:48 +0100

    top bar removed

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import sys
 import random
 import time
+import numpy as np
+from PIL.ImageQt import ImageQt
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
 import make_screenshot
 from screeninfo import get_monitors
 
+from desktopmagic.screengrab_win32 import getDisplaysAsImages
+
 
 class Main_Menu(QtWidgets.QWidget):
+    DELAY_TIMES = ['None', '1s', '2s', '3s', '4s', '5s', '10s']
+    INSTANCES = []
     def __init__(self):
         super().__init__()
         self.monitors_info()
         self.UI()
 
-
     def UI(self):   
         self.layout = QtWidgets.QVBoxLayout()
 
         self.new_snip = QtWidgets.QPushButton("New") 
-        self.new_snip.clicked.connect(self.new_snipping_window)
+        self.new_snip.clicked.connect(self.new_snipping_windows)
 
-        self.delay_list = ['None', '1s', '2s', '3s', '4s', '5s', '10s']
         self.delay_combo = QComboBox(self)
-        self.delay_combo.addItems(self.delay_list)
+        self.delay_combo.addItems(Main_Menu.DELAY_TIMES)
 
         self.layout.addWidget(self.new_snip, 1)
         self.layout.addWidget(self.delay_combo, 0)
@@ -39,29 +44,64 @@ class Main_Menu(QtWidgets.QWidget):
                         
             self.monitors_resolution.append(monitor)
 
-    def new_snipping_window(self):
-        self.close()
-        if self.delay_combo.currentText() != 'None':
+    def new_snipping_windows(self):
+        # self.close()
+
+        if self.delay_combo.currentText() == 'None':
+            time.sleep(0.3)
+            # print("none")
+        else:
             time.sleep(int(self.delay_combo.currentText()[:-1]))
         
 
         # TODO: place to take printscreen
-        from desktopmagic.screengrab_win32 import getDisplaysAsImages
+        self.shots = []
+        for displayNumber, im in enumerate(getDisplaysAsImages(), 1):
+            # im.save(f'picures/_{displayNumber}.png', format='png')
+            # im.show()
+            # self.qim = ImageQt(im)
+            # self.shots.append(self.qim)
+            self.shots.append(im)
+            # print(im)
+            # if displayNumber == 1:
+            #     break
+        # print(self.shots)
+        # pictures[0].show()
 
-        # print(self.monitors_resolution)
-        
-        self.make_screenshot_instance = []
-        [self.make_screenshot_instance.append(make_screenshot.MainWindow(monitor_number=self.monitors_resolution[i][0], #ScreenshotWidget
-                                                                         screen_width=self.monitors_resolution[i][1], 
-                                                                         screen_height=self.monitors_resolution[i][2])) 
-                                                                         for i, monitor in enumerate(self.monitors_resolution)]
+        [Main_Menu.INSTANCES.append(make_screenshot.MainWindow(
+                                    monitor_number=self.monitors_resolution[i][0], #ScreenshotWidget
+                                    screen_width=self.monitors_resolution[i][1], 
+                                    screen_height=self.monitors_resolution[i][2],
+                                    shot=self.shots[i]),)
+                                    for i, monitor in enumerate(self.monitors_resolution)]
+        # print(Main_Menu.INSTANCES)
+
+        # self.close() #test
+
+        # self.make_screenshot_instance = []
+        # [self.make_screenshot_instance.append(make_screenshot.MainWindow(
+        #                             monitor_number=self.monitors_resolution[i][0], #ScreenshotWidget
+        #                             screen_width=self.monitors_resolution[i][1], 
+        #                             screen_height=self.monitors_resolution[i][2],
+        #                             shot=self.shots[i]),)
+        #                             for i, monitor in enumerate(self.monitors_resolution)]
+        # print(self.make_screenshot_instance)
+
+        # self.monitor = 0
+        # self.make_screenshot_instance.append(make_screenshot.MainWindow(
+        #                             monitor_number=self.monitors_resolution[self.monitor][0], #ScreenshotWidget
+        #                             screen_width=self.monitors_resolution[self.monitor][1], 
+        #                             screen_height=self.monitors_resolution[self.monitor][2],
+        #                             shot=self.shots[self.monitor]))
 
 
 if __name__ == "__main__":
+
     app = QtWidgets.QApplication(sys.argv)
 
     widget = Main_Menu()
     widget.resize(300, 200)
     widget.show()
+    print(widget.INSTANCES)
 
     sys.exit(app.exec_())
--- make_screenshot.py
+++ make_screenshot.py
@@ -1,26 +1,28 @@
 import numpy as np
 import sys
 import cv2
-from PIL import ImageGrab
+import PIL
+from PIL import ImageGrab, ImageQt, ImageFilter, ImageEnhance
+# from PIL.ImageQt import ImageQt
+
 from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtCore import Qt
+# from PyQt5.QtCore import Qt
 from screeninfo import get_monitors
+import main 
 
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, parent=None, monitor_number=None, screen_width=None, screen_height=None):
-        # TODO Change monitor variables to *args
+    def __init__(self, parent=None, monitor_number=None, screen_width=None, screen_height=None, shot=None):
+        # TODO Change monitor variables to *args, maybe
         super().__init__(parent)
-        QtWidgets.QMainWindow.__init__(self, parent)
+        # QtWidgets.QMainWindow.__init__(self, parent) probably not needed
 
         self.monitor_number = int(monitor_number) - 1 #Qt is starting reading screen number from 0
         self.screen_width = int(screen_width)
         self.screen_height = int(screen_height)
-
-        
-        self.screenshot_tool_widget = Screenshot_tool(parent=self)
-        self.setCentralWidget(self.screenshot_tool_widget)
-
-        # self.QMenu.setTitle('sdawdawdawd')
+        self.shot = shot
+        # no idea why it is here
+        # self.screenshot_tool_widget = Screenshot_tool(parent=self)
+        # self.setCentralWidget(self.screenshot_tool_widget)
 
         self.starter()
 
@@ -28,19 +30,54 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setGeometry(0, 0, self.screen_width, self.screen_height)
         # self.setWindowTitle(' ')
         self.begin = QtCore.QPoint()
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint) #no title bar
         # self.setWindowOpacity(0.3) window transparency
-
         self.monitor = QtWidgets.QDesktopWidget().screenGeometry(self.monitor_number)
         self.move(self.monitor.left(), self.monitor.top())
+        # self.shot = self.shot.point(lambda p: p * 0.9)
+        # # ImageFilter.GaussianBlur(radius=2) 
+        # self.shot = self.shot.filter(ImageFilter.GaussianBlur(radius=1))
+        # enhancer = ImageEnhance.Contrast(self.shot)
+        # self.shot = enhancer.enhance(4.0)
 
-        # self.QMenu.setWindowTitle('asd')
+        self.qimage = ImageQt.ImageQt(self.shot)
+
+        label = QtWidgets.QLabel(self)
+        pixmap = QtGui.QPixmap.fromImage(self.qimage)
+        label.setPixmap(pixmap)
+        self.setCentralWidget(label)
+
+        QtWidgets.QApplication.setOverrideCursor(QtGui.QCursor(QtCore.Qt.CrossCursor))
+
         self.show()
 
-class Screenshot_tool(QtWidgets.QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        QtWidgets.QWidget.__init__(self, parent)
+    def keyPressEvent(self, event): #q for exit
+        if event.key() == QtCore.Qt.Key_Q:
+            # print('Quit')
+            self.close()
+        event.accept()
+
+    def mousePressEvent(self, event):
+        self.start = event.pos()
+        # print(main.widget.INSTANCES)
+        print(self.start)
+        # self.end = self.start
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        self.end = event.pos()
+        print(self.end)
+        self.update()
+
+
+
+
+
+# class Screenshot_tool(QtWidgets.QWidget):
+#     def __init__(self, parent=None):
+#         super().__init__(parent)
+#         QtWidgets.QWidget.__init__(self, parent)
 
 
 
